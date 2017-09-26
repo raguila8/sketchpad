9d9165a1d1b546660de65cd894dc0eea9ec0ae0a
Rodrigo Aguilar <rodrigoaguilar887@gmail.com>
2017-09-25 21:15:23 -0700

    add download and color picker functionality

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -4,19 +4,76 @@ $(document).ready(function() {
 
 function main() {
   //var color = "black";
+	//setBorder();
+	setSpectrum();
+	setContainerHeight();
+	//setDrawingLight();
   grid();
+	openHelp();
+	closeHelp();
   resize();
   hoverEffect();
+	registerClick();
   changeColor();
 	resetTiles();
 	downloadSketch();
+	/*$("#colors").spectrum({
+    color: "#000",
+		change: function(color) {
+    	color = color.toHexString(); // #ff0000
+		},
+		replacerClassName: 'awesome',
+		
+  });*/
 }
 
-var color = "black";
+var squareColor = "black";
+var drawing = false;
+
+function setSpectrum() {
+	$("#colors").spectrum({
+    color: "#000",
+		change: function(color) {
+    	squareColor = color.toHexString();
+			$('#container').css("border", "2px solid " + squareColor);
+
+		},
+		replacerClassName: 'awesome'
+  });
+
+}
+
+/*
+function setBorder() {
+	$border = $("#border");
+	viewportWidth = $(window).width();
+	viewportHeight = $(window).height();
+	$border.height((viewportHeight - $(".container-fluid").height()) / 1.21);
+	$border.width(viewportWidth / 2.5);
+}
+*/
+/*
+function setDrawingLight() {
+	borderHeight = $('#border').height();
+	containerHeight = $('#container').height();
+	headerHeight = $('#border h3').height();
+	bottomBorder = borderHeight - containerHeight - headerHeight;
+	$('#drawing').css("bottom", bottomBorder / 2 - 10 + "px");
+}
+*/
+
+
+function setContainerHeight() {
+	$container = $("#container");
+	$container.width(.35 * $(window).width());
+	$container.height($container.width());
+}
 
 // makes 16 by 16 grid
-function grid(num = 16) { 
-  var dim = 640 / num;
+function grid(num = 16) {
+	$container = $("#container");
+	var dim = $container.width() / num;
+  //var dim = 640 / num;
   for (var i = 0; i < num; i++) {
     for (var j = 0; j < num; j++) {
       $square = $("<div class=\"square\"></div>");
@@ -35,7 +92,9 @@ function resetTiles() {
 
 function hoverEffect() {
   $("div").on("mouseenter", '.square', function() {
-    $(this).css('background-color', color);
+		if (drawing) {
+    	$(this).css('background-color', squareColor);
+		}
   });
 }
 
@@ -47,11 +106,20 @@ function resize() {
     grid(num);
   });
 }
-
+/*
 function changeColor() {
-  $(".colors li").click(function() {
+  $("#colors").click(function() {
     color = $(this).attr("id");
+		color = $('#colors').spectrum("get").toHexString();
+		console.log($("#colors").spectrum("get").toHexString());
+		$('#container').css("border", "3px solid " + color);
   });
+}
+*/
+function changeColor() {
+	$("#colors").on("move", function() {
+		console.log("here");
+	});
 }
 
 function downloadURI(uri, name) {
@@ -75,5 +143,34 @@ function downloadSketch() {
 				downloadURI("data:" + mySketch, "youImage.png");
 			}
 		});
+	});
+}
+
+function registerClick() {
+	$("#container").on("click", function() {
+		$drawing = $('#drawing');
+		drawing = !drawing;
+		if (drawing) {
+			$drawing.css('background-color', 'green');
+		} else {
+			$drawing.css('background-color', 'red');
+		}
+	});
+}
+
+function openHelp() {
+	$('.help-handle').click(function() {
+		$help = $('.help');
+		$help.width("250px");
+		$('.main').css("margin-left", "250px");
+		$('body').css("background-color", "rgba(0,0,0,0.4)");
+	});
+}
+
+function closeHelp() {
+	$(".closebtn").on("click", function() {
+		$(".help").width("0");
+  	$('.main').css("margin-left", "0");
+  	$('body').css("backgroundColor", "#474e5d");
 	});
 }
